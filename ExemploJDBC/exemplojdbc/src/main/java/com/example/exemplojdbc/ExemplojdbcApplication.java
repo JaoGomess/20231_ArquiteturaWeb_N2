49ecdcc7b62ab3f129abcbcd760c0314ed1e2373
Rafael Moreno <rafaelmorenosax@gmail.com>
2023-03-16 19:27:28 -0300

    jdbc

---
+++ ExemploJDBC/exemplojdbc/src/main/java/com/example/exemplojdbc/ExemplojdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.exemplojdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplojdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemplojdbcApplication.class, args);
+	}
+
+}
