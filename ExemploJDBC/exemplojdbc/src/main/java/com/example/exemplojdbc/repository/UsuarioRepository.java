104c5c5950a68b4654bfde32268eb2b80c9a0552
Rafael Moreno <rafaelmorenosax@gmail.com>
2023-03-16 21:21:23 -0300

    mapper

---
+++ ExemploJDBC/exemplojdbc/src/main/java/com/example/exemplojdbc/repository/UsuarioRepository.java
@@ -0,0 +1,22 @@
+package com.example.exemplojdbc.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.exemplojdbc.model.Usuario;
+
+@Repository
+public class UsuarioRepository {
+    private static String SELECT_ALL = "SELECT * FROM USUARIO";
+    private static String INSERT = "INSERT INTO USUARIO (nome,email,senha) VALUES(?,?,?)";
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Usuario inserir(Usuario usu) {
+        jdbcTemplate.update(INSERT, new Object[] {
+                usu.getNome(), usu.getEmail(), usu.getSenha()
+        });
+        return usu;
+    }
+}
