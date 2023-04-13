0ea9e027894156c647605d27dde78389da0a29e4
Rafael Moreno <rafaelmorenosax@gmail.com>
2023-04-12 21:31:58 -0300

    Exemplo N:N

---
+++ exemplospringdatajpa/src/main/java/com/example/exemplospringdatajpa/model/Pessoa.java
@@ -0,0 +1,19 @@
+package com.example.exemplospringdatajpa.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Pessoa {
+    @Id
+    private Integer id;
+    @ManyToMany
+    @JoinTable(name = "pessoa_curso", joinColumns = { @JoinColumn(name = "pessoa_id") }, inverseJoinColumns = {
+            @JoinColumn(name = "curso_id") })
+    List<Curso2> cursos;
+}
