d478918f0c31ac1fabda17431af1bcea734712b3
Rafael Moreno <rafaelmorenosax@gmail.com>
2023-05-17 22:22:55 -0300

    JWT

---
+++ ExemploApiCompleta/src/main/java/com/example/exemplospringdatajpa/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.exemplospringdatajpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.exemplospringdatajpa.models.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Usuario findByEmail(String email);
+}
